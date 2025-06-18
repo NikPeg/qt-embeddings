43980810f901f18eac4f35466702e454dda9d08d
NikPeg <peganov.nik@gmail.com>
2025-06-18 19:22:45 +0300

    no_warn

--- s6_filter_tatar_words.py
+++ s6_filter_tatar_words.py
@@ -31,8 +31,6 @@ def is_tatar_word(word, tokenizer, model):
         **inputs,
         max_new_tokens=1,
         do_sample=False,
-        temperature=0.01,
-        top_p=0.95,
         eos_token_id=tokenizer.eos_token_id,
         pad_token_id=tokenizer.eos_token_id
     )
