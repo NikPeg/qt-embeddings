84f18f94ce4312c04d47895200fd1bd5362a0ab6
NikPeg <peganov.nik@gmail.com>
2025-06-18 19:30:01 +0300

    temp

--- s6_filter_tatar_words.py
+++ s6_filter_tatar_words.py
@@ -29,8 +29,10 @@ def is_tatar_word(word, tokenizer, model):
     inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
     outputs = model.generate(
         **inputs,
-        max_new_tokens=8,  # увеличено
-        do_sample=False,
+        max_new_tokens=16,
+        do_sample=True,
+        temperature=0.7,
+        top_p=0.95,
         eos_token_id=tokenizer.eos_token_id,
         pad_token_id=tokenizer.eos_token_id
     )
